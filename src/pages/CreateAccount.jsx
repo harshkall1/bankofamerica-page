9f82e4176b01969c78b89b9c60428e1d56540732
9678anu <anupriyavarshney9678@gmail.com>
2025-03-22 15:47:05 +0530

    updated account and index

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -13,6 +13,8 @@ const CreateAccount = () => {
         phoneNo: "",
         password: "",
         amount: 0,
+        bankname: "BankofAmerica"
+
     });
 
     // State for loading
@@ -51,6 +53,7 @@ const CreateAccount = () => {
                 phoneNo: "",
                 password: "",
                 amount: 0,
+                bankname: "BankofAmerica"
             });
 
             // Navigate to a confirmation page
